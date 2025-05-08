a868ec05cf7662e9ff4725ff77261a91cb1ae471
Yori <bagusbagus0331@gmail.com>
2025-05-08 13:33:26 +0700

    Update configuration.nix

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,7 +1,6 @@
 { config, ... }: {
   imports = [
     # Mostly system related configuration
-    ../../nixos/nvidia.nix # CHANGEME: Remove this line if you don't have an Nvidia GPU
     ../../nixos/audio.nix
     ../../nixos/bluetooth.nix
     ../../nixos/fonts.nix
